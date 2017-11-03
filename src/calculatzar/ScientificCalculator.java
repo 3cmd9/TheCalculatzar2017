38e933c827d88a27552afa0a26c471a18a219906
069986115 <069986115@109R030573XJQ22.EDU.YRDSB.NET>
2017-11-03 11:32:51 -0400

    Merge origin/master
    Conflicts:
    	src/calculatzar/ScientificCalculator.java
    
    Added distance and pythagorean methods

--- src/calculatzar/ScientificCalculator.java
+++ src/calculatzar/ScientificCalculator.java
@@ -11,4 +11,11 @@ package calculatzar;
  */
 public class ScientificCalculator {
     
+    public static double pythagorean(double a, double b){
+        return distance(0,0,a,b);
+    }
+    
+    public static double distance (double x1,double y1,double x2, double y2){
+        return Math.sqrt((Math.pow(x1,2)-Math.pow(x2,2))+(Math.pow(y1,2)-Math.pow(y2,2)));
+    }
 }
