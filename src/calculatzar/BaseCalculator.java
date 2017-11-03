2daa7298562b52a09cefcd2930c954550461ba23
072935638 <072935638@109R030573W9Q22.EDU.YRDSB.NET>
2017-11-03 11:23:29 -0400

    Added list of methods in comments

--- src/calculatzar/BaseCalculator.java
+++ src/calculatzar/BaseCalculator.java
@@ -7,6 +7,16 @@ package calculatzar;
 
 /**
  *Remember: All Methods Static, ask Richard for method Signature
+ * 
+ * Methods:
+ *  convertToBase(int base, int num);               Luke
+ *  convertToDecimal(int currentBase, int num);     Leo
+ *  add (int num1, int num2, int base);             Richard
+ *  subtract(int num1, int num2, int base);         Kaiyi
+ *  multiply(int num1, int num2, int base_;         Luke
+ *  divide(int num1, int num2 int base);            Leo
+ *  square(int num, int base);                      Richard
+ *  squareRoot(int, num, int base);                 Kaiyi
  */
 public class BaseCalculator {
     
