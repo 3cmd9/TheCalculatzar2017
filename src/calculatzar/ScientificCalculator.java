391844a55dc3e9f0814eb7266315feca9c60ed68
072698376 <072698376@109R0305737KQ22.EDU.YRDSB.NET>
2017-11-03 11:39:59 -0400

    Sup

--- src/calculatzar/ScientificCalculator.java
+++ src/calculatzar/ScientificCalculator.java
@@ -10,5 +10,24 @@ package calculatzar;
  * @author dunca
  */
 public class ScientificCalculator {
+
+    public double slope(double x, double y, double z, double a) {
+        double tempA = a - y;
+        double tempB = z - x;
+        return tempA / tempB;
+    }
+
+    public String equation(double x, double y, double z, double a) {
+        double m = slope(x, y, z, a);
+        double b = y - (m * x);
+        return "y = " + m + "x + " + b;
+    }
     
+    //For linear functions.
+    public double tangentLineSlope(double x, double y) {
+        
+    }
+    
+    //For quadratic functions.
+    public double tangentLineSlope(double x, double y, double z, double a, )
 }
