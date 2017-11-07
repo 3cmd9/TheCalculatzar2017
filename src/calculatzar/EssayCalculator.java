99111bcafb5b8ccb3793d81e0cdedd7d71ad8bfc
348948605 <348948605@109R030573YHQ22.EDU.YRDSB.NET>
2017-11-07 10:51:33 -0500

    Merge origin/master
    Conflicts:
    	src/calculatzar/EssayCalculator.java

--- src/calculatzar/EssayCalculator.java
+++ src/calculatzar/EssayCalculator.java
@@ -19,14 +19,19 @@ public class EssayCalculator {
         essay = ess;
     }
     
-    String tempEssay= essay
-            for (int i=0; i<tempEssay.length(); i++){
-    if (tempEssay.charAt(i)!=','){
-        essay+=tempEssay.charAt(i);
+    public ArrayList<String> noPuncSplitWords(){
+    String tempEssay= essay;
+    String lowercase=tempEssay.toLowerCase();
+        ArrayList<String> words=new ArrayList<>();
+    for (int i=0; i<tempEssay.length(); i++){
+            if (lowercase.charAt(i)>=97&&lowercase.charAt(i)<=122){
+            essay+=tempEssay.charAt(i);
+            }
+        words = splitEssay(' ');
+        essay = tempEssay;
     }
-    ArrayList<String> words = splitEssay(' ')
-            essay = tempEssay;
-}
+        return words;
+    }
     
     public double avgWordLength(){
         ArrayList<String> words = splitEssay(' ');
@@ -51,9 +56,11 @@ public class EssayCalculator {
         }
         return ess;
     }
-    public String repeatingWords (){
-        ArrayList<String> writing = splitEssay(' ');
-        for (int i=0; i<)
+    public String repeatingWord (){//returns the most repetetive word
+        ArrayList<String> writing = noPuncSplitWords();
+        for (int i=0; i<writing.size();i++){
+            
+        }
     }
 
 }
